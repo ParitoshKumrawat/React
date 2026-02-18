d0aa238987302ad0e912b2358d01119d181ef397
Paritosh <paritoshkumrawat@gmail.com>
2026-02-18 09:07:02 +0530

    App.jsx-update

--- Props/src/App.jsx
+++ Props/src/App.jsx
@@ -14,6 +14,7 @@ const App = () => {
         age={20}
         img="https://plus.unsplash.com/premium_photo-1768288662582-24c46f53d290?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
       />
+      <Card name="Rohit" age={30} />
     </div>
   );
 };
